f930fb3f930b2e8d243a22373bb7efcbd483d2af
muriel-maths <murielmaths@gmail.com>
2016-11-10 15:15:47 +0100

    Inner Joins

---
+++ inner_join.sql
@@ -0,0 +1,41 @@
+/*Intersection between sets*/
+SELECT 
+	customer.customer_id,
+	first_name,
+	last_name,
+	email,
+	amount,
+	payment_date
+FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+SELECT customer.customer_id,
+	first_name,
+	last_name,
+	email,
+	amount,
+	payment_date
+FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+ORDER BY customer.customer_id;
+
+
+SELECT customer.customer_id,
+	first_name,
+	last_name,
+	email,
+	amount,
+	payment_date
+FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+WHERE customer.customer_id = 2;
+
+SELECT customer.customer_id,
+	first_name,
+	last_name,
+	email,
+	amount,
+	payment_date
+FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+WHERE first_name LIKE 'A%';
